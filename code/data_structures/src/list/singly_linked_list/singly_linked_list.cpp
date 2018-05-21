24f30a50a957e59a1e61526e5a5e518f439095a0
Tyler <tcgbell@gmail.com>
2018-05-21 16:12:55 -0300

    encapsulated and leak fix
    added member function push and memory management encapsulation to eliminate leaks.
    added robustness to instantiation and argument passing for efficiency

--- code/data_structures/src/list/singly_linked_list/singly_linked_list.cpp
+++ code/data_structures/src/list/singly_linked_list/singly_linked_list.cpp
@@ -1,36 +1,71 @@
-// C++ program to print reverse of a linked list
+// C++ program to print the reverse of a singly linked list
 #include <iostream>
 
-class Node {
-public:
-    int data;
+template<typename T>
+class Node
+{
+private:
+    T data;
     Node *next;
+public:
+    void print_reverse();
+    void push(const T&); //lvalue
+    void push(const T&&); //rvalue
+    Node(const T&);
+    Node(const T&&);
+    ~Node();
 };
 
-void printReverse(Node *head)
+template<typename T> Node<T>::Node(const T& data_) : data{data_}, next{nullptr}
+{}
+template<typename T> Node<T>::Node(const T&& data_) : data{data_}, next{nullptr}
+{}
+
+template<typename T> Node<T>::~Node()
 {
-    if (head == nullptr)
-        return;
-    printReverse(head->next);
-    std::cout << head->data << " ";
+ if(next) delete next;
 }
 
-void push(Node **head_ref, char new_data)
+template<typename T> void Node<T>::push(const T &data_)
 {
-    auto new_node = (Node *) malloc(sizeof(Node));
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
+ Node *newNode = new Node<T>{data_};
+ Node *current_node = this;
+
+ while(current_node->next != nullptr)
+  current_node = current_node->next;
+
+ current_node->next = newNode;
+}
+template<typename T> void Node<T>::push(const T &&data_)
+{
+ Node *newNode = new Node<T>{data_};
+ Node *current_node = this;
+
+ while(current_node->next != nullptr)
+  current_node = current_node->next;
+
+ current_node->next = newNode;
 }
 
+template<typename T> void Node<T>::print_reverse()
+{
+    if(next)
+     next->print_reverse();
+    std::cout << data << " ";
+}
+
+
+/*
+This sample main instantiates a list of ints {4,3,2,1} using the Node<T>::push member function.
+*/
 int main()
 {
-    Node *head = nullptr;
-    push(&head, 4);
-    push(&head, 3);
-    push(&head, 2);
-    push(&head, 1);
+    Node<int> *list{new Node<int>(4)};
+    list->push(3);
+    list->push(2);
+    list->push(1);
+    list->print_reverse();
+    delete list;
 
-    printReverse(head);
     return 0;
 }
